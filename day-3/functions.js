543e7546745560a54aaca79080e4196c644ffdc4
sowmya-rambilli <2211cs010492@mallareddyuniversity.ac.in>
2025-01-09 10:21:29 +0530

    pushing the code to repo

---
+++ day-3/functions.js
@@ -0,0 +1,78 @@
+// function addtoCart(){
+//     alert("Product is added to the cart")
+// }
+
+// function demo(){
+//     console.log("function is executing.....");
+// }
+// demo(); 
+
+// with parameters and arguments
+//let a = 10;  a = parameter , 10 = arguments
+
+// 5,6
+// 8,9
+// 3,4
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(5,6);
+// add(8,9);
+// add(3,4);
+
+// function test(){
+//     console.log("named function is executing....");
+// }
+// test();
+
+// //anonymous function
+// function(){
+
+// }
+// ();
+
+// let x = function(){
+//     console.log("anonymous is not executing but executing with variable");
+// }
+// // console.log(x);
+// x();
+
+//IIFE(immediate invoke function expression)
+// (
+//     function(){
+//         console.log("IIFE");
+
+//     }
+// )();
+
+
+// // ! arrow function
+// function demo(){
+
+// }
+// demo();
+
+// let x =_=>console.log("Arrow function");
+// x();
+
+// function demo{
+//     console.log("hello");
+// }
+// demo();
+
+// let x = a => console.log(a);
+// x(4);
+
+
+//! implicit returns and explicit returns.
+
+// function test1(a,b){
+//     return a+b;
+// }
+// console.log(test1(5,5));
+
+// let x = (a,b) => {return a*b};
+// console.log(x(5,5));
+
+// let y = (a,b) => a+b;
+// console.log(y(100,100));
