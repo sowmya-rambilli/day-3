3b966a6faa12c0d02b8c5019d59aea7256de5269
sowmya-rambilli <2211cs010492@mallareddyuniversity.ac.in>
2025-01-23 18:46:22 +0530

    pushing the code to repo

--- async.js
+++ async.js
@@ -51,3 +51,39 @@
 //     response.catch(err=>console.log(err))
 // }
 // fetchUsers();
+
+// ASYNC & AWAIT !!
+
+// function demo()
+// {
+//     console.log("start");
+//     console.log(10);
+//     console.log(20);
+//     return;
+//     console.log(30);
+//     console.log("end");
+// }
+// demo();
+
+// let p = new Promise((resolve , reject)=>{
+//     setTimeout(()=>{
+//         resolve("success")
+//     },4000);
+// });
+
+// async function demo(){
+//     console.log("start");
+//     let x =await p;
+//     console.log(x);
+//     console.log("end");
+// }
+// demo();
+
+// async function fetchUsers(){
+//     let response =await fetch("https://jsonplaceholder.typicode.com/users")
+//     let data =await response.json();
+//     console.log(data);
+// };
+// fetchUsers();
+
+// console.log(window)
