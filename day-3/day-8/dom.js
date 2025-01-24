f143dac1455e55a0de156acafa3aef3821e14869
sowmya-rambilli <2211cs010492@mallareddyuniversity.ac.in>
2025-01-24 14:18:40 +0530

    pushing the code to repo

---
+++ day-3/day-8/dom.js
@@ -0,0 +1,47 @@
+//  let element = document.getElementById("demo");
+//  element.innerHTML = "DOM"
+//  console.log(element);
+
+//  let test = document.getElementById("test");
+//  test.innerHTML = "<h1>Header</h1>"
+//  console.log(test);
+
+
+// let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor = "doggerblue"
+// console.log(ele);
+// console.log(Array.isArray(ele));
+
+
+// let x = [...ele];
+// console.log(x, Array.isArray(x));
+// x.map(element=>{
+//     console.log(element);
+//     element.style.backgroundColor = "yellow";
+//     element.style.color = "tomato";
+//     element.style.textTransform = "uppercase";
+// });
+
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+// [...ele.].map(element);
+// console.log(element .innerText);
+
+
+// let element = document.querySelector("#demo");
+// console.log(element);
+// let element = document.querySelector(".test");
+// console.log(element);
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+// let demo = document.getElementById("demo");
+// demo.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     alert("hi")
+// });
+
+// let demo = document.getElementById("demo");
+// demo.addEventListener("dbclick",()=>{
+//     console.log("button click");
--- day-3/dom.js
+++ day-3/dom.js
@@ -1,1 +1,23 @@
-let element = document.getElementById
+// let element = document.getElementById("demo");
+// element.innerHTML = "DOM"
+// console.log(element);
+
+// let test = document.getElementById("test");
+// test.innerHTML = "<h1>Header</h1>"
+// console.log(test);
+
+
+let ele = document.getElementsByClassName("test");
+ele[0].style.backgroundColor = "doggerblue"
+console.log(ele);
+console.log(Array.isArray(ele));
+
+
+let x = [...ele];
+console.log(x, Array.isArray(x));
+x.map(element=>{
+    console.log(element);
+    element.style.backgroundColor = "tomato";
+    element.style.color = "white";
+    element.style.textTransform = "uppercase";
+})
